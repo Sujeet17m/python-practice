35289a67edd06f6139aa6c494d2ad6d84d058287
Sujeet17m <sujeet17das@gmail.com>
2025-06-17 10:58:51 +0530

    different

--- index.py
+++ index.py
@@ -4,4 +4,4 @@ print('Dear Mrs Badger')
 print('Your son ' + name + ' is failing ' + failed_subjects + ' subjects.')
 print(name + '  will need to redo ' + failed_subjects + '  courses.')
 name="Eric"
-print(name + '  is doing well in geography.')
+print(name + '  is doing well in geography.') 
