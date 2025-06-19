ff51c465f424a7fbbb27b7b859f19f776f7e6254
Sujeet17m <sujeet17das@gmail.com>
2025-06-19 10:25:01 +0530

    if the string has digit then using capitalize don't work , you can use title() method to capitalize it

--- index.py
+++ index.py
@@ -1,7 +1,23 @@
-failed_subjects="2"
-name='John'
-print('Dear Mrs Badger')
-print('Your son ' + name + ' is failing ' + failed_subjects + ' subjects.')
-print(name + '  will need to redo ' + failed_subjects + '  courses.')
-name="Eric"
-print(name + '  is doing well in geography.') 
+# a=2
+# b=3
+# print('Addition : ', a + b)
+# print('Subtraction : ', a - b)
+# print('Multiplication : ', a * b)
+# print('Division (float) : ', a / b)
+# print('Division (floor) : ', a // b)
+# print('Modulus : ', a % b)
+# print('Exponent : ', a ** b)
+
+# msg = '''"1 welcome ring to tyler" 
+# "1 welcome ring to tyler" 
+# "1 welcome ring to tyler" 
+# "1 welcome ring to tyler"'''
+# print(msg.title())
+# print(msg[::-1])
+
+name = 'TERRY'
+color = 'RED'
+msg = '['+ name + '] loves the color ' + color.lower() + '!'
+print(msg) 
+msg1 = f'[{name.capitalize()} loves the color {color.lower()}]!'
+print(msg1)
